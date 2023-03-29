658c64165729a6565f032dd593ed1e895987e7f1
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-30 00:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ setInterval(()=>{
 	let sec = now.getSeconds();
 	let min = now.getMinutes();
 	let hour = now.getHours();
-	document.getElementsByClassName("hour-hand")[0].style.transform = "rotate("+((hour % 12) + (min / 60)) * 30+"deg)";
-	document.getElementsByClassName("min-hand")[0].style.transform = "rotate("+(min+(sec/60))*6+"deg)";
+	// document.getElementsByClassName("hour-hand")[0].style.transform = "rotate("+((hour % 12) + (min / 60)) * 30+"deg)";
+	// document.getElementsByClassName("min-hand")[0].style.transform = "rotate("+(min+(sec/60))*6+"deg)";
 	document.getElementsByClassName("second-hand")[0].style.transform = "rotate("+hour*6+"deg)";
 },1000)
 }
